0967c3d919bd757cd6e07f019a8ab3cb2c795cd0
Javier-Jimenez99 <javijj99@gmail.com>
2021-06-14 23:43:51 +0200

    add docstrings

--- CNN_stress/stress_model.py
+++ CNN_stress/stress_model.py
@@ -1,4 +1,4 @@
-from typing import Union
+from typing import Optional, Union
 from pathlib import Path
 from keras.applications.resnet50 import (
     ResNet50,
@@ -21,31 +21,61 @@ MODEL_TYPES = {
 
 class CNN:
     def __init__(
-        self, model_path: Union[str, Path] = None, base_model: Union[str, None] = None
+        self,
+        model_path: Optional[Union[str, Path]] = None,
+        base_model: Optional[str] = None,
     ):
+        """Initialize a new CNN model.
+
+        Parameters
+        ----------
+        model_path : Optional[Union[str, Path]]
+            Path to model save folder, by default None
+            If it is `None`, a `ResNet50v2` with weights of `imagenet` is loaded.
+        base_model : Optional[str], optional
+            Base model to load weights on it, by default None.
+            If it is `None`, a local saved model will be loaded using `model_path`.
+        """
         if model_path is not None:
             self.MODEL = self.load(model_path, base_model)
         else:
             self.MODEL = self.load()
 
-    def save(self, model_path: Union[str, Path] = "imagenet"):
+    def save(self, model_path: Union[str, Path]):
+        """Save the architecture and the weights of the model.
+
+        Parameters
+        ----------
+        model_path : Union[str, Path]
+            Path to the folder where the model will be saved.
+        """
         model_json = self.MODEL.to_json()
 
-        with open(f"{model_path}.json", "w") as json_file:
+        with open(f"{model_path}/model_arquitecture.json", "w") as json_file:
             json_file.write(model_json)
 
-        self.MODEL.save_weights(f"{model_path}.h5")
+        self.MODEL.save_weights(f"{model_path}/model_weights.h5")
 
     def load(
         self,
         model_path: Union[str, Path] = "imagenet",
-        base_model: Union[str, None] = "ResNet50v2",
+        base_model: Optional[str] = "ResNet50v2",
     ):
+        """Load the architecture and the weights of a model.
+
+        Parameters
+        ----------
+        model_path : Union[str, Path]
+            Path to the folder where the model is saved.
+        base_model : Optional[str], optional
+            Base model to load weights on it, by default None.
+            If it is `None`, a local saved model will be loaded using `model_path`.
+        """
         if base_model is None:
-            with open(f"{model_path}.json", "r") as f:
+            with open(f"{model_path}/model_arquitecture.json", "r") as f:
                 self.MODEL = f.read()
 
-            self.MODEL.load_weights(f"{model_path}.h5")
+            self.MODEL.load_weights(f"{model_path}/model_weights.h5")
 
         else:
             self.MODEL = MODEL_TYPES[base_model](weights=model_path)
